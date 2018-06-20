e432693aeac097f623e388ff899abfd2b92dd45b
wlhebut172 <584529403@qq.com>
2018-06-20 23:18:10 +0800

    初次提交

---
+++ src/main/webapp/js/test.js
@@ -0,0 +1,49 @@
+
+	//changeImage();
+	
+	function de(){
+		
+		var cars=new Array();
+		cars[0]="aodi";
+		cars[1]="baoma";
+		cars[2]="hema";
+		//console.info(cars.pop());
+		//console.info(cars.push("dama"));
+		//console.info(cars.pop());
+		//console.info(cars.forEach());
+		
+		/*for(var c in cars){
+			console.info(cars[c]);
+		}*/
+		/*cars.forEach(function(e){
+			console.info(e);
+		});*/
+		
+		/*var text="hello";
+		//find
+		var num= text.indexOf('e', 0);*/
+		//alert(num);
+		/*var ts= text.slice(0, 2);
+		var upperCase=text.toUpperCase();
+		alert(upperCase);
+		alert(ts);*/
+		/*var p= new Object();
+		p.firstname='bill';
+		p.lastname='gates';
+		p.age=56;
+		p.eyecolor='blue';
+		console.info(p.age);*/
+		
+		/*var sum=new Object();
+		sum.a=1;
+		sum.b=2;
+		function summ(sum){
+			return sum.a+sum.b;
+		}
+		var summ2=new Object();
+		summ2.a=10;
+		summ2.b=20;
+		var mm= summ(summ2);
+		alert(mm);*/
+	}
+	
